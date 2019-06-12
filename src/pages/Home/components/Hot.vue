0b43e91e70155439aa89878bf9c51bb9e6f3ac19
gmYuan <ygm121351@163.com>
2019-06-12 22:57:36 +0800

    add

--- src/pages/Home/components/Hot.vue
+++ src/pages/Home/components/Hot.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hot-wrap">
     <div class="info">
-      <img src alt>
-      <span>本周热门榜单</span>
+      <img class="hot-img" src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png" alt="热门榜单">
+      <span class="desc">本周热门榜单</span>
 
-      <a href="javascript:;" class="all-info">
+      <a href="javascript:;" class="more">
         <span>全部榜单</span>
-        <i>>></i>
+        <i class="iconfont more-right"> &#xe6a3; </i>
       </a>
     </div>
   </div>
@@ -28,7 +28,39 @@ export default {
 
   .info {
     outline: 1px solid red;
+    position: relative;
     padding: 24px 0 26px;
+
+    .hot-img {
+      display: inline-block;
+      overflow: hidden;
+      width: 30px;
+      height: 30px;
+      margin-left: 20px;
+      vertical-align: top;
+    }
+    .desc {
+      display: inline-block;
+      margin-left: 8px;
+      height: 44px;
+      line-height: 44px;
+      color: #212121;
+      font-size: 32px;
+    }
+    .more {
+      position: absolute;
+      top: 36px;
+      right: 32px;
+      color: #616161;
+      font-size: 24px;
+      line-height: 30px;
+      .more-right {
+        margin-left: -8px;
+        font-size: 32px;
+        color:#9A9A9A;
+        vertical-align: top;
+      }
+    }
   }
 }
 </style>
