1178ee8ea9fb5cff82d7766e16e9f6bb9ed96e1a
gmYuan <ygm121351@gmail.com>
2019-06-13 23:29:09 +0800

    完成 本周热门

--- src/pages/Home/components/Hot.vue
+++ src/pages/Home/components/Hot.vue
@@ -21,9 +21,9 @@
       >
         <a href="javascript:;" class="full-link">
           <div class="rate-wrap"
-          v-if="index < 3"
+            v-if=" index < 3"
           >
-            <img class="rate-tag" src="#" alt="#">
+            <img class="rate-tag" :src="item.topSrc" alt="#">
           </div>
 
           <div class="place-wrap">
@@ -34,8 +34,7 @@
 
           <div class="place-price">
             <span class="price-category">
-              ¥
-              <em class="price-num">{{item.price}}</em>
+              ¥<em class="price-num">{{item.price}}</em>
             </span>
             起
           </div>
@@ -60,21 +59,24 @@ export default {
           url:
             "http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_250x250_57ac4faa.jpg",
           desc: "上海迪士尼乐园",
-          price: 399
+          price: 399,
+          topSrc: 'http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png'
         },
         {
           id: "002",
           url:
             "http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_250x250_963fe222.jpg",
           desc: "上海欢乐谷",
-          price: 195
+          price: 195,
+          topSrc: 'http://img1.qunarzz.com/piao/fusion/1710/2d/36d0c4adaebbbc02.png',
         },
         {
           id: "003",
           url:
             "http://img1.qunarzz.com/sight/p0/1904/d5/d579b03a110d75a8a3.img.jpg_250x250_cf952d13.jpg",
           desc: "上海野生动物园",
-          price: 118
+          price: 118,
+          topSrc: 'http://img1.qunarzz.com/piao/fusion/1710/67/edc47ffef9e96b02.png'
         },
         {
           id: "004",
@@ -97,21 +99,29 @@ export default {
           desc: "黄浦江游览（十六铺码头）",
           price: 60
         }
-      ]
-    };
-  }
-};
+      ],
+      
+    }
+  },
+
+  
+
+
+}
 </script>
 
 
 <style lang="scss" scoped>
+
+@import '@scss/mixins.scss';
+
 .hot-wrap {
   margin-top: 20px;
   width: 100%;
   background: #fff;
 
   .info {
-    outline: 1px solid red;
+   
     position: relative;
     padding: 24px 0 26px;
 
@@ -153,30 +163,67 @@ export default {
     display: flex;
     flex-wrap: nowrap;
     overflow-x: scroll;
+
     .place-item {
       position: relative;
       display: inline-block;
       padding: 6px 0 20px;
+      margin: 0 6px;
       width: 200px;
+
       .full-link {
         display: block;
         width: 100%;
         height: 100%;
 
         .rate-wrap {
+          position: absolute;
+          top: 0;
+          left: 0;
+          z-index: 1;
+          overflow: hidden;
+          width: 84px;
+          height: 40px;
 
+          .rate-tag {
+            display: block;
+            width: 100%;
+          }
         }
 
-        
         .place-wrap {
           overflow: hidden;
           width: 200px;
           height: 0;
-          padding-bottom: 100%;
+          padding-bottom: 100%;    //防止网速不好时的页面抖动
           .place {
             display: block;
             width: 100%;
           }
+        }
+
+        .place-desc {
+          margin-top: 12px;
+          color: #212121;
+          font-size: 24px;
+          line-height: 32px;
+          text-align: center;
+          @include ellipse();
+        }
+
+        .place-price {
+          color: #616161;
+          font-size: 24px;
+          line-height: 36px;
+          text-align: center;
+
+          .price-category {
+            color: #ff8300;
+            .price-num {
+              font-size: 28px;
+            }
+          }
+
         }
       }
     }
